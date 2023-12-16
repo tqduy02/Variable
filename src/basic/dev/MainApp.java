501d29aa6f6f83375425ce6efb96e4247aea9692
tqduy02 <98021569+tqduy02@users.noreply.github.com>
2023-12-16 07:45:56 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,22 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		/*
+		 * Khai bao bien
+		 */
+		String firstName = "Tran Quoc";
+		String lastName = "Duy";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten: ");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+
+		// Khai bao hang. Hang phai viet hoa
+		final float PI = 3.14f;
+	}
+}
